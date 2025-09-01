d1883d7002e6b0ec36dc7ae681068f9f6f96c8cf
qwed86 <www.120944258@gmail.com>
2025-09-01 17:28:39 +0800

    2025.9.1

---
+++ C++输出数据.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main()
+{   
+cout << "姓名："<<"爱莉希雅;"<<"年龄："<<17<<";体重："<<54.8<<"kg(不许四舍五入)"
+          <<"；性别："<<'X'<<endl;
+cout<<"David";
+std::cout << __cplusplus << "\n";
+}
---
+++ demo01.cpp
@@ -0,0 +1,10 @@
+//包含头文件
+#include<iostream>
+//main函数，程序执行的入口
+int main()
+{   // 在终端输出hello world！并换行
+    std::cout << "hello world!\n";
+    // std::cout向控制台输出内容的指令
+    // << 输出的运算符
+    // ; c++语句结束符
+}
